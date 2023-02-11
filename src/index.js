15bde9b50622b1a87fbf4312f7fd977e1b97a833
Sai Kiran <chitturidurgasatyasaikiran@gmail.com>
2023-02-11 11:37:18 +0530

    commit
    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./todolist.css";
-import TodoList from "./App";
+import TodoList from "./TodoList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

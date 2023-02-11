62f3a74656c8439bf975a6022d5aca5f3ae9b68e
Sai Kiran <102981731+Keyrun1227@users.noreply.github.com>
2023-02-11 11:32:13 +0530

    Add files via upload

---
+++ src/Alert.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+const Alert = ({ message, type, setAlert }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert({ status: false, type: "", message: "" });
+    }, 3000);
+  }, []);
+
+  return <div className={`alert ${type}`}>{message}</div>;
+};
+
+export default Alert;
